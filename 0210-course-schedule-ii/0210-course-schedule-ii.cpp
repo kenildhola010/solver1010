599295fc78ae0acff6c6f509139cec45c876e66b
Kenil Dhola <118338465+kenildhola010@users.noreply.github.com>
2026-06-19 15:33:32 +0530

    Time: 14 ms (6.16%), Space: 19.9 MB (5.53%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& v) {
+
+        vector<int> in(V,0);
+        unordered_map<int,vector<int>> mp;
+        queue<int> q;
+
+        vector<int> ans;
+
+        for(auto i : v){
+            mp[i[1]].push_back(i[0]);
+        }
+        for(auto i : mp){
+            for(auto j : i.second){
+                in[j]++;
+            }
+        }
+
+        for(int i=0; i<V; i++){
+            if(in[i] == 0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto i : mp[node]){
+                in[i]--;
+                if(in[i] == 0) q.push(i);
+            }
+        }
+        if(ans.size() != V) return {};
+
+        return ans;
+        
+    }
+};
