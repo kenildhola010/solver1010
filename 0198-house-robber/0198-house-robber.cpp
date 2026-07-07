cfb9a2be6d347266a013995bcb727ef7d11eb2bd
Kenil Dhola <118338465+kenildhola010@users.noreply.github.com>
2026-07-07 18:40:21 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-
-    int fun(int id, vector<int> &v, vector<int> &dp){
-        if(id == 0) return v[id];
-        if(id < 0) return 0;
-
-        if(dp[id] != -1) return dp[id];
-
-        int take  = v[id];
-        if(id-2 >= 0)
-         take = v[id] + fun(id-2, v,dp);
-        int nott = fun(id-1,v,dp);
-
-        return dp[id] = max(take, nott);
-    }
     int rob(vector<int>& v) {
         int n = v.size();
 
-        vector<int> dp(n,-1);
+        vector<int> dp(n, -1);
 
-        return fun(n-1, v, dp);
+        dp[0] = v[0];
+
+        for (int i = 1; i < n; i++) {
+            int take = v[i];
+            if (i - 2 >= 0)
+                take = v[i] + dp[i - 2];
+            int nott = dp[i - 1];
+
+            dp[i] = max(nott, take);
+        }
+
+        return dp[n - 1];
     }
 };
